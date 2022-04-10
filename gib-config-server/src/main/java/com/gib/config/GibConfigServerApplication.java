ec45a2c2a87c0f7a7502e7c6a10e790e8b1ee67e
rajsekharmahapatro <rajsekharmahapatro@gmail.com>
2022-04-10 15:12:33 +0530

    first commit- creating project

---
+++ gib-config-server/src/main/java/com/gib/config/GibConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.gib.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GibConfigServerApplication.class, args);
+	}
+
+}
---
+++ gib-discovery/src/main/java/com/gib/discovery/GibDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.gib.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GibDiscoveryApplication.class, args);
+	}
+
+}
